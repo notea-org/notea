1d2ec5cfd0e844360516d19cd9215515a140aa17
liqingwei <liqingwei@shimo.im>
2021-03-03 21:49:44 +0800

    feat: mobile search ui

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -3,16 +3,18 @@ import { FC } from 'react'
 import { Modal } from '@material-ui/core'
 import SearchList from './search-list'
 import SearchInput from './search-input'
+import { UIState } from 'containers/ui'
 
 const Search: FC = () => {
   const { isOpen, closeModal } = SearchState.useContainer()
+  const { ua } = UIState.useContainer()
 
   return (
     <Modal
       open={isOpen}
       onClose={closeModal}
-      style={{ top: '10vh' }}
-      className="w-1/3 m-auto"
+      style={{ top: ua.isMobileOnly ? 0 : '10vh' }}
+      className="w-full m-auto md:w-1/2 lg:w-1/3"
     >
       <div className="bg-gray-50 text-gray-800 outline-none rounded overflow-auto">
         <SearchInput />
--- components/search/search-list.tsx
+++ components/search/search-list.tsx
@@ -3,19 +3,19 @@ import { FC } from 'react'
 import SearchItem from './search-item'
 import styled from 'styled-components'
 
-const StyledUL = styled.ul`
-  max-height: 76vh;
+const StyledList = styled.ul`
+  max-height: calc(80vh - 40px);
 `
 
 const SearchList: FC = () => {
   const { list, keyword } = SearchState.useContainer()
 
   return list?.length ? (
-    <StyledUL className="border-t border-gray-200 overflow-auto">
+    <StyledList className="border-t border-gray-100 overflow-auto">
       {list?.map((note) => (
         <SearchItem keyword={keyword} note={note} key={note.id} />
       ))}
-    </StyledUL>
+    </StyledList>
   ) : null
 }
 
