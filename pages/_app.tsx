fe2f98cafeb6ca394b5483e88a8a80c74f1d478b
liqingwei <liqingwei@shimo.im>
2021-04-30 09:52:08 +0800

    chore: bye panelbear

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import { AppProps } from 'next/app'
-import { usePanelbear } from 'utils/panelbear'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // Load Panelbear only once during the app lifecycle
-  usePanelbear('ByKvhHO8nQK', {
-    // Uncomment to allow sending events on localhost, and log to console too.
-    debug: process.env.NODE_ENV !== 'production',
-  })
   return <Component {...pageProps} />
 }
 
