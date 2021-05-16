4a5b999bac2d7f928f0f1d7c9261d7ef07f59a8f
liqingwei <liqingwei@shimo.im>
2021-05-16 12:12:48 +0800

    Updates

---
+++ _next/static/WuhmFeT-Xk7Z5xNkEgv6w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-dd108341475595066cd1.js"],"/_error":["static/chunks/pages/_error-3fb9ad2ef9d653f137e5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
