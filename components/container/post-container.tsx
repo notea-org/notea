f805c4666bddf20cf5c87ba7afa567a630235f69
liqingwei <liqingwei@shimo.im>
2021-06-06 12:18:13 +0800

    fix: sharing page

--- components/container/post-container.tsx
+++ components/container/post-container.tsx
@@ -29,130 +29,134 @@ export const PostContainer: FC<{
     })
   }, [injection, note])
 
-  return (
-    <article
-      className={classNames('prose mx-auto pb-10 prose-sm px-4 md:px-0', {
+  const articleClassName = useMemo(
+    () =>
+      classNames('prose mx-auto pb-10 prose-sm px-4 md:px-0', {
         'md:prose-2xl': !small,
-      })}
-    >
-      <header>
-        <h1 className="pt-10">{note?.title ?? t('Untitled')}</h1>
-      </header>
-      <main
-        dangerouslySetInnerHTML={{
-          __html: post,
-        }}
-      ></main>
-      <style jsx>{`
-        .prose :glboal(img) {
-          margin: auto;
-        }
+      }),
+    [small]
+  )
 
-        .prose :glboal([title='left-50']) {
-          float: left;
-          width: 50%;
-          margin-right: 2em;
-          margin-bottom: 1em;
-          clear: initial;
-        }
+  return (
+    <>
+      <article className={articleClassName}>
+        <header>
+          <h1 className="pt-10">{note?.title ?? t('Untitled')}</h1>
+        </header>
+        <main
+          dangerouslySetInnerHTML={{
+            __html: post,
+          }}
+        ></main>
+        <style jsx>{`
+          .prose :glboal(img) {
+            margin: auto;
+          }
 
-        .prose :glboal([title='right-50']) {
-          float: right;
-          width: 50%;
-          margin-left: 2em;
-          margin-bottom: 1em;
-          clear: initial;
-        }
+          .prose :glboal([title='left-50']) {
+            float: left;
+            width: 50%;
+            margin-right: 2em;
+            margin-bottom: 1em;
+            clear: initial;
+          }
 
-        .prose :glboal(figcaption) {
-          text-align: center;
-        }
+          .prose :glboal([title='right-50']) {
+            float: right;
+            width: 50%;
+            margin-left: 2em;
+            margin-bottom: 1em;
+            clear: initial;
+          }
 
-        .prose :global(.task-list-item) {
-          padding-left: 0;
-        }
+          .prose :glboal(figcaption) {
+            text-align: center;
+          }
 
-        .prose :global(.task-list-item::before) {
-          content: none;
-        }
+          .prose :global(.task-list-item) {
+            padding-left: 0;
+          }
 
-        .prose :global(.task-list-item label) {
-          margin-left: 6px;
-        }
+          .prose :global(.task-list-item::before) {
+            content: none;
+          }
 
-        .prose :global(.notice) {
-          display: flex;
-          align-items: center;
-          background: ${editorTheme.noticeInfoBackground};
-          color: ${editorTheme.noticeInfoText};
-          border-radius: 4px;
-          padding: 8px 16px;
-          margin: 8px 0;
-        }
+          .prose :global(.task-list-item label) {
+            margin-left: 6px;
+          }
 
-        .prose :global(.notice *) {
-          margin: 0;
-          color: currentColor;
-        }
+          .prose :global(.notice) {
+            display: flex;
+            align-items: center;
+            background: ${editorTheme.noticeInfoBackground};
+            color: ${editorTheme.noticeInfoText};
+            border-radius: 4px;
+            padding: 8px 16px;
+            margin: 8px 0;
+          }
 
-        .prose :global(.notice .icon) {
-          width: 1.5em;
-          height: 1.5em;
-          align-self: flex-start;
-          margin-right: 4px;
-          position: relative;
-          top: 1px;
-        }
+          .prose :global(.notice *) {
+            margin: 0;
+            color: currentColor;
+          }
 
-        .prose :global(.notice svg) {
-          width: 100%;
-          height: 100%;
-        }
+          .prose :global(.notice .icon) {
+            width: 1.5em;
+            height: 1.5em;
+            align-self: flex-start;
+            margin-right: 4px;
+            position: relative;
+            top: 1px;
+          }
 
-        .prose :global(.notice .content) {
-          flex-grow: 1;
-        }
+          .prose :global(.notice svg) {
+            width: 100%;
+            height: 100%;
+          }
 
-        .prose :global(.notice a) {
-          color: ${editorTheme.noticeInfoText};
-        }
-        .prose :global(.notice a:not(.heading-name)) {
-          text-decoration: underline;
-        }
+          .prose :global(.notice .content) {
+            flex-grow: 1;
+          }
 
-        .prose :global(.notice-tip) {
-          background: ${editorTheme.noticeTipBackground};
-          color: ${editorTheme.noticeTipText};
-        }
+          .prose :global(.notice a) {
+            color: ${editorTheme.noticeInfoText};
+          }
+          .prose :global(.notice a:not(.heading-name)) {
+            text-decoration: underline;
+          }
 
-        .prose :global(.notice-tip a) {
-          color: ${editorTheme.noticeTipText};
-        }
-        .prose :global(.notice-warning) {
-          background: ${editorTheme.noticeWarningBackground};
-          color: ${editorTheme.noticeWarningText};
-        }
+          .prose :global(.notice-tip) {
+            background: ${editorTheme.noticeTipBackground};
+            color: ${editorTheme.noticeTipText};
+          }
 
-        .prose: global(.notice-warning a) {
-          color: ${editorTheme.noticeWarningText};
-        }
+          .prose :global(.notice-tip a) {
+            color: ${editorTheme.noticeTipText};
+          }
+          .prose :global(.notice-warning) {
+            background: ${editorTheme.noticeWarningBackground};
+            color: ${editorTheme.noticeWarningText};
+          }
 
-        .prose :global(table *) {
-          margin: 0;
-        }
+          .prose: global(.notice-warning a) {
+            color: ${editorTheme.noticeWarningText};
+          }
 
-        footer a {
-          color: currentColor;
-          font-weight: normal;
-        }
-      `}</style>
-
+          .prose :global(table *) {
+            margin: 0;
+          }
+        `}</style>
+      </article>
       {small ? null : (
         <>
           {injection ? (
-            <InnerHTML id="snippet-injection" html={injectionHTML} />
+            <InnerHTML
+              id="snippet-injection"
+              className={articleClassName}
+              html={injectionHTML}
+            />
           ) : null}
-          <footer className="text-gray-300 text-center my-20 py-10 text-sm">
+          <footer className="text-gray-300 text-center my-20 text-sm">
             Built with{' '}
             <a
               href="https://cinwell.com/notea/"
@@ -164,6 +168,6 @@ export const PostContainer: FC<{
           </footer>
         </>
       )}
-    </article>
+    </>
   )
 }
