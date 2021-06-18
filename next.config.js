1d55b19816ed43357a1347f0e10a64cefd74e81c
liqingwei <liqingwei@shimo.im>
2021-06-18 21:59:18 +0800

    chore: upgrade nextjs, react, rich-markdown-editor

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
-  future: {
-    /**
-     * FIXME
-     * https://github.com/netlify/netlify-plugin-nextjs/issues/209
-     */
-    webpack5: process.env.NETLIFY ? false : true,
-  },
-
   target: process.env.NETLIFY ? 'serverless' : 'server',
 
   pwa: {
