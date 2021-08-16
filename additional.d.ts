d392e16300ce19c7b4022fddf4de959bec7388cd
liqingwei <liqingwei@shimo.im>
2021-08-16 10:45:12 +0800

    chore: fix tsconfig

---
+++ additional.d.ts
@@ -0,0 +1,9 @@
+declare module 'react-split'
+
+declare module 'remove-markdown'
+
+declare module 'outline-icons'
+
+declare module '@headwayapp/react-widget'
+
+declare module 'markdown-link-extractor'
