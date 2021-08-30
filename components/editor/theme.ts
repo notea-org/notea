66cfd5d22a4909e20f756590c6d812e434721993
liqingwei <liqingwei@shimo.im>
2021-08-30 10:03:14 +0800

    fix: editor dark theme

--- components/editor/theme.ts
+++ components/editor/theme.ts
@@ -1,19 +1,19 @@
 import { useTheme } from 'next-themes'
 import { theme } from 'rich-markdown-editor'
+import { light, dark } from 'rich-markdown-editor/dist/theme'
 
-export const editorTheme: typeof theme = {
-  ...theme,
+export const darkTheme: typeof theme = {
+  ...dark,
   background: 'inherit',
   text: 'inherit',
   fontFamily: 'inherit',
 }
 
-export const darkTheme: typeof theme = {
-  ...editorTheme,
-}
-
 export const lightTheme: typeof theme = {
-  ...editorTheme,
+  ...light,
+  background: 'inherit',
+  text: 'inherit',
+  fontFamily: 'inherit',
 }
 
 export const useEditorTheme = () => {
