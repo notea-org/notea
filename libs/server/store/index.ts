7b9538dbdac7af87850d63074075e9f6c98c9cbb
Andreas Wittig <andreas@widdix.de>
2022-01-03 11:06:58 +0100

    Adding support for AWS IAM role credentials

--- libs/server/store/index.ts
+++ libs/server/store/index.ts
@@ -3,8 +3,8 @@ import { StoreS3 } from './providers/s3'
 
 export function createStore() {
   return new StoreS3({
-    accessKey: getEnv('STORE_ACCESS_KEY', undefined, true),
-    secretKey: getEnv('STORE_SECRET_KEY', undefined, true),
+    accessKey: getEnv('STORE_ACCESS_KEY'),
+    secretKey: getEnv('STORE_SECRET_KEY'),
     endPoint: getEnv('STORE_END_POINT'),
     bucket: getEnv('STORE_BUCKET', 'notea'),
     region: getEnv('STORE_REGION', 'us-east-1'),
