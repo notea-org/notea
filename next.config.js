94fb16696f14b228f8a911fbff20009ca2b18f2d
tecc <tecc@tecc.me>
2022-08-12 13:32:50 +0200

    change(next): Next config now has a wrapper for PWA config
    change(next): The configuration now provides a middleware webpack configuration function before passing it to withPWA's webpack function.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 const withPWA = require('next-pwa');
 const cache = require('./scripts/cache');
 
-module.exports = withPWA({
+const base = withPWA({
     target: process.env.NETLIFY ? 'serverless' : 'server',
 
     pwa: {
         disable: process.env.NODE_ENV === 'development',
         dest: 'public',
         runtimeCaching: cache
-    },
+    }
+})
+
+const wp = base.webpack;
+module.exports = Object.assign(base, {
+    webpack(config, options) {
+        // In case it's required to change the webpack config
+        return wp(config, options);
+    }
 });
