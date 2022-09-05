dbb6e75810e32610c602ff7d6fc95bf5ad55b1c4
tecc <tecc@tecc.me>
2022-09-05 16:40:28 +0200

    chore(deps)!: Update to Next.js 12 and other dependencies
    Next.js@12: Next.js 12 should provide better performance and stuffs.
    
    .babelrc: .babelrc has been removed to take advantage of Next.js' SWC backend (which offers faster build times) - if the file was still present, Babel would automatically be chosen over SWC.
    
    .yarnclean: Commented out the "images" yarnclean directive, as it was causing issues with Next.js.
    
    next-pwa: next-pwa has been updated, and next.config.js has been updated to use its newer API.
    
    react: React has been updated to React 18. The @types package could not use the latest version due to some issues with the package itself.
    
    rich-markdown-editor: rich-markdown-editor could not be updated due to issues with theming.
    
    BREAKING-CHANGE: Next.js and React have been upgraded to their later versions.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,18 @@
-const withPWA = require('next-pwa');
+const nextPWA = require('next-pwa');
 const cache = require('./scripts/cache');
 
-const base = withPWA({
-    target: process.env.NETLIFY ? 'serverless' : 'server',
+const withPWA = nextPWA({
+    // target: process.env.NETLIFY ? 'serverless' : 'server',
+    // mode: process.env.NODE_ENV ?? 'development',
+    dest: 'public'
+});
+
+module.exports = withPWA({
+    swcMinify: true,
 
     pwa: {
         disable: process.env.NODE_ENV === 'development',
         dest: 'public',
-        runtimeCaching: cache,
-    },
-});
-
-const wp = base.webpack;
-module.exports = Object.assign(base, {
-    webpack(config, options) {
-        // In case it's required to change the webpack config
-        return wp(config, options);
-    },
+        runtimeCaching: cache
+    }
 });
