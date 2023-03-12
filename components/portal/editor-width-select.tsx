62fa2eb228a6a99528974c530d045978bbd1e7c0
tecc <tecc@tecc.me>
2023-03-12 09:55:11 +0100

    change: Highlight active editor width setting in setting popup

--- components/portal/editor-width-select.tsx
+++ components/portal/editor-width-select.tsx
@@ -4,6 +4,7 @@ import PortalState from 'libs/web/state/portal';
 import { Menu, MenuItem } from '@material-ui/core';
 import { EDITOR_SIZE } from 'libs/shared/meta';
 import NoteState from 'libs/web/state/note';
+import UIState from 'libs/web/state/ui';
 
 interface EditorWidthSelectItem {
     text: string;
@@ -15,6 +16,9 @@ const EditorWidthSelect: FC = () => {
     const {
         editorWidthSelect: { close, anchor, data, visible },
     } = PortalState.useContainer();
+    const {
+        settings: { settings },
+    } = UIState.useContainer();
     const { mutateNote } = NoteState.useContainer();
 
     const items: Array<EditorWidthSelectItem> = [
@@ -42,6 +46,8 @@ const EditorWidthSelect: FC = () => {
         }
     };
 
+    const editorWidth = data?.editorsize ?? settings.editorsize;
+
     return (
         <Menu
             anchorEl={anchor}
@@ -53,7 +59,9 @@ const EditorWidthSelect: FC = () => {
         >
             {items.map((item) =>
                 <MenuItem key={item.value} onClick={() => setTo(item.value)}>
-                    <span className="text-xs">{item.text}</span>
+                    <span className={`text-xs ${editorWidth == item.value ? 'font-bold' : ''}`}>
+                        {item.text}
+                    </span>
                 </MenuItem>
             )}
         </Menu>
